0e5a376d86a4d2131812ff6463c2d300537cc17f
khot-abhishek <abhishekrkhot@gmail.com>
2022-11-13 19:10:45 +0530

    feat: add bootstrap classes to form fields

--- CRUD_Ajax/enroll/forms.py
+++ CRUD_Ajax/enroll/forms.py
@@ -7,3 +7,8 @@ class StudentRegistration(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name', 'email', 'password']
+        widgets = {
+            'name' : forms.TextInput(attrs={'class':'form-control', 'id': 'name_id'}),
+            'email' : forms.EmailInput(attrs={'class':'form-control', 'id': 'email_id'}),
+            'password' : forms.PasswordInput(attrs={'class':'form-control', 'id' : 'password_id'})
+        }
