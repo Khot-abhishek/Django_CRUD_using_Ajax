c4f34060e779782941868633a77fe58c26cbe013
khot-abhishek <abhishekrkhot@gmail.com>
2022-11-14 22:31:01 +0530

    feat: create views to handle GET & POST request
    create a home() view for the initial GET request & a save_data()
    to handle the post request by AJAX call and return all data with msg
    & create coresponding URL path in urls.py of main project directory

--- CRUD_Ajax/enroll/views.py
+++ CRUD_Ajax/enroll/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
+def home(request):
+    context = {
+        'form':StudentRegistration(),
+        'students': User.objects.all()
+    }
+    return render(request, 'enroll/home.html', context)
+
+def save_data(request):
+    if request.method == 'POST':
+        form = StudentRegistration(request.POST)
+        if form.is_valid():
+            form.save()
+            all_users = User.objects.values()
+    # queryset objects cannot be json sirealized so it is converted to a list inside context
+            context = {
+                'msg':'form saved successfully',
+                'students': list(all_users)        # <-----
+            }
+            return JsonResponse(context, )
+        else:
+            all_users = User.objects.values()
+            context = {
+                'msg':'Unable to save Form',
+                'students': list(all_users)
+            }
+            return JsonResponse(context)
+            
