e4843ed9d0b295beb5971aa15f38ff7bb5b987bc
khot-abhishek <abhishekrkhot@gmail.com>
2022-11-15 16:52:59 +0530

    create a edit view to retrive data to ajax call for update
    the deit view takes th ID and returns the object data in dictionary
    format through the "edit/" URl path, This view does not make changes in
    data

--- CRUD_Ajax/enroll/views.py
+++ CRUD_Ajax/enroll/views.py
@@ -39,3 +39,12 @@ def delete_data(request):
         user.delete()
         return JsonResponse({'status':1})
     return JsonResponse({'status':0})
+
+
+# view to populate the form fields when edit button is clicked
+def edit_data(request):
+    if request.method == "POST":
+        id = request.POST.get('edit_id')
+        student = User.objects.get(pk=id)
+        student_data = {'id':student.id, 'name':student.name, 'email':student.email, 'password':student.password}
+        return JsonResponse(student_data)
